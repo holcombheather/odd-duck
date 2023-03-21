2d49fe5c30a6a44df26ebe4a42992ec6ec15c153
Heather Holcomb <holcombheather@gmail.com>
2023-03-20 22:12:35 -0700

    Validated functionality for user story 1and 2, initial attempt at user story 3 and 4

--- app.js
+++ app.js
@@ -1,71 +1,65 @@
-const state = [];
-const roundsOfVoting = 5;
+// validated with first user story
 
-function Image(name,source) {
+// Constructor function to create product objects
+function Product(name, imagePath) {
   this.name = name;
-  this.timesClicked = 0;
-  this.source = source;
+  this.imagePath = imagePath;
+  this.timesShown = 0;
 }
 
-state.push(new Image(''))
-state.push(new Image(''))
-state.push(new Image(''))
-state.push(new Image(''))
+// Array of product objects
+let products = [
+  new Product('Product 1', 'img/bag.jpg'),
+  new Product('Product 2', 'img/banana.jpg'),
+  new Product('Product 3', 'img/bathroom.jpg'),
+  new Product('Product 4', 'img/boots.jpg'),
+  new Product('Product 5', 'img/breakfast.jpg'),
+  new Product('Product 6', 'img/bubblegum.jpg'),
+  new Product('Product 7', 'img/chair.jpg'),
+  new Product('Product 8', 'img/cthulhu.jpg'),
+  new Product('Product 9', 'img/dog-duck.jpg'),
+  new Product('Product 10', 'img/dragon.jpg'),
+  new Product('Product 11', 'img/pen.jpg'),
+  new Product('Product 12', 'img/pet-sweep.jpg'),
+  new Product('Product 12', 'img/scissors.jpg'),
+  new Product('Product 12', 'img/shark.jpg'),
+  new Product('Product 12', 'img/sweep.png'),
+  new Product('Product 12', 'img/tauntaun.jpg'),
+  new Product('Product 12', 'img/unicorn.jpg'),
+  new Product('Product 12', 'img/water-can.jpg'),
+  new Product('Product 12', 'img/wine-glass.jpg'),
+];
 
-let imgEls = document.querySelectorAll('img'); //array like thing that filled 
-let voteTrackerEl = document.getElementById('vote-tracker');
+// Function to generate three unique product images
+function generateImages() {
+  let product1 = products[Math.floor(Math.random() * products.length)];
+  let product2 = products[Math.floor(Math.random() * products.length)];
+  let product3 = products[Math.floor(Math.random() * products.length)];
 
-console.log("Currently rendered images", imgEls);
+  // Check if products are unique and generate new ones if necessary
+  while (product1 === product2 || product1 === product3 || product2 === product3) {
+    product1 = products[Math.floor(Math.random() * products.length)];
+    product2 = products[Math.floor(Math.random() * products.length)];
+    product3 = products[Math.floor(Math.random() * products.length)];
+  }
 
-console.log('Current State', state);
+  // Update timesShown property for each product
+  product1.timesShown++;
+  product2.timesShown++;
+  product3.timesShown++;
 
-//render our first images
-// imgEls[0].src = state[0].source;
-// imgEls[0].id = state[0].name;
-// imgEls[1].src = state[1].source;
-// imgEls[1].id = state[1].name;
-
-
-function generateRandomGoat() {
-    return Math.floor(Math.random() * state.length);
+  // Display images on the page
+  document.getElementById('product1').src = product1.imagePath;
+  document.getElementById('product2').src = product2.imagePath;
+  document.getElementById('product3').src = product3.imagePath;
 }
 
-function renderGoats() {
-    console.log(imgEls)
-
-    //find some goats from state
-    let goat1 = generateRandomGoat();
-    let goat2 = generateRandomGoat();
-    console.log('Goats to rerender', imgEls, goat1, goat2);
-    while (goat1.name ==)
-
-    imgEls[0].src = state[0].source;
-    imgEls[0].id = state[0].name;
-    goat1.timesShown += 1;
-    imgEls[1].src = state[1].source;
-    imgEls[1].id = state[1].name;
-    goat2.timesShown += 1;
-}
-
-renderGoats();
-
-let eventId = voteTrackerEl.addEventListener('click', function(event) {
-    console.log(event.target); //event.target always represents the exact element where an event occured
-
-    //identify which image was clicked on?
-    let goatThaWasClicked = event.target.dispatchEvent;
-    state.forEach(image => {
-        if (image.name === goatThaWasClicked) {
-            image.timesClicked += 1; //mutation of an object
-        }
-    });
-    console.log('Updated State', state);
-    //re-render updated state
-
-    if (roundsOfVoting) {
-        renderGoats();
-        roundsOfVoting--;
-    } else {
-        voteTrackerEl.removeEventListener('click', eventId);
-    }
+// Add event listener to image container
+document.getElementById('image-container').addEventListener('click', function(event) {
+  if (event.target.classList.contains('product-image')) {
+    generateImages();
+  }
 });
+
+// Initial image generation
+generateImages();
