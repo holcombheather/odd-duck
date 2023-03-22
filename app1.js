5eb3aa4475fc6f779d0ccb3eda064680d5c9bfc1
Heather Holcomb <holcombheather@gmail.com>
2023-03-21 20:02:44 -0700

    Validated User Story 4

--- app1.js
+++ app1.js
@@ -1,9 +1,6 @@
 const state = []; //why do we call it state? 
 let roundsOfVoting = 25;
 
-// let currentRound = 0;
-// let activeProducts = [];
-
 function Product(name, source) {
   this.name = name;
   this.source = source;
@@ -40,11 +37,10 @@ console.log('Currently rendered images', imgEls);
 
 console.log('Current State', state);
 
-//verify our images render
+// verify our images render
 // imgEls[0].src = state[0].source;
 generateImages();
 
-// Function to randomly generate three unique product images
 function generateRandom() {
   return Math.floor(Math.random() * state.length);
 }
@@ -89,129 +85,26 @@ function handleImageClick(event) {
     roundsOfVoting--;
   } else {
     voteTrackerEl.removeEventListener('click', handleImageClick);
+    let buttonEl = document.getElementById('view-results-button');
+    console.log('This is the button', buttonEl);
+    buttonEl.addEventListener('click', generateResults);
+    alert('You have reached the max number of votes');
   }
 }
 
 voteTrackerEl.addEventListener('click', handleImageClick);
 
 
+function generateResults(event) {
+  let buttonClicked = event.target.id;
+  state.forEach(product => {
+    let listItemEl = document.createElement('li');
+    let resultContainer = document.getElementById('results-list');
+    resultContainer.appendChild(listItemEl);
+    listItemEl.innerHTML = `${product.name} had ${product.timesClicked} votes, and was seen ${product.timesShown} times.`;
+    product.timesClicked;
+    product.timesShown;
+  });
+  buttonClicked.addEventListener('click', generateResults);
+}
 
-
-
-// generateImages();
-
-
-// function handleImageClick(event) {
-//   if (event.target.classList.contains('product-image')) {
-//     // Find the clicked product and update its timesClicked property
-//     let clickedProduct = state.find(function(product) {
-//       return product.imagePath === event.target.src;
-//     });
-//     clickedProduct.timesClicked++;
-//   }
-// }
-
-
-
-// document.getElementById('image-container').addEventListener('click', function(event) {
-//   if (event.target.classList.contains('product-image') && currentRound < numRounds) {
-//     handleImageClick(event);
-//     generateImages();
-//     currentRound++;
-
-//     if (currentRound === numRounds) {
-//       // Voting has ended, you can perform any post-voting actions here.
-//       // For example, remove the event listener or display a message.
-//       console.log("You have exceeded the number of votes allowed.");
-//     }
-//   }
-// });
-
-
-
-
-
-
-
-
-
-// // Constructor function to create product objects
-// function Product(name, imagePath) {
-//   this.name = name;
-//   this.imagePath = imagePath;
-//   this.timesShown = 0;
-//   this.timesClicked = 0;
-// }
-
-// // Array of product objects
-// let products = [
-//   new Product('bag', 'img/bag.jpg'),
-//   new Product('banana', 'img/banana.jpg'),
-//   new Product('bathroom', 'img/bathroom.jpg'),
-//   new Product('boots', 'img/boots.jpg'),
-//   new Product('breakfast', 'img/breakfast.jpg'),
-//   new Product('bubblegum', 'img/bubblegum.jpg'),
-//   new Product('chair', 'img/chair.jpg'),
-//   new Product('cthulhu', 'img/cthulhu.jpg'),
-//   new Product('dog-duck', 'img/dog-duck.jpg'),
-//   new Product('dragon', 'img/dragon.jpg'),
-//   new Product('pen', 'img/pen.jpg'),
-//   new Product('pet-sweep', 'img/pet-sweep.jpg'),
-//   new Product('scissors', 'img/scissors.jpg'),
-//   new Product('shark', 'img/shark.jpg'),
-//   new Product('sweep', 'img/sweep.png'),
-//   new Product('tauntaun', 'img/tauntaun.jpg'),
-//   new Product('unicorn', 'img/unicorn.jpg'),
-//   new Product('water-can', 'img/water-can.jpg'),
-//   new Product('wine-glass', 'img/wine-glass.jpg')
-// ];
-
-// // Function to generate three unique product images
-// function generateImages() {
-//     let product1 = products[Math.floor(Math.random() * products.length)];
-//     let product2 = products[Math.floor(Math.random() * products.length)];
-//     let product3 = products[Math.floor(Math.random() * products.length)];
-
-//     // Check if products are unique and generate new ones if necessary
-//     while (product1 === product2 || product1 === product3 || product2 === product3) {
-//       product1 = products[Math.floor(Math.random() * products.length)];
-//       product2 = products[Math.floor(Math.random() * products.length)];
-//       product3 = products[Math.floor(Math.random() * products.length)];
-//     }
-
-//     // Update timesShown property for each product
-//     product1.timesShown++;
-//     product2.timesShown++;
-//     product3.timesShown++;
-
-//     // Display images on the page
-//     document.getElementById('product1').src = product1.imagePath;
-//     document.getElementById('product2').src = product2.imagePath;
-//     document.getElementById('product3').src = product3.imagePath;
-//   }
-
-//   // Add event listener to image container
-//   document.getElementById('image-container').addEventListener('click', function(event) {
-//     if (event.target.classList.contains('product-image')) {
-//       generateImages();
-//     }
-//   });
-
-//   // Initial image generation
-//   generateImages();
-
-
-// // Number of voting rounds
-// let numRounds = 25;
-
-// // Array of products currently being considered
-// let activeProducts = [];
-
-// // Function to generate three unique product images
-// function generateImages() {
-//   // Clear event listeners and display results button if voting rounds have ended
-//   if (numRounds === 0) {
-//     document.getElementById('image-container').removeEventListener('click', handleImageClick);
-//     document.getElementById('view-results-button').style;
-//   }
-// }
